d5c70c858a64a782c42563de6589bcd0ac5bc362
sungkihwan <singahu@naver.com>
2022-01-14 05:05:51 +0900

    세션인증 로그인

--- server.js
+++ server.js
@@ -1,62 +1,118 @@
-// express 세팅
+// app.use() : 미들웨어 사용 (req -> 미들웨어 -> res 요청과 응답 사이에 동작하는 코드)
+// npm install dotenv -> 환경변수 사용
+require('dotenv').config()
+
+// npm install express (framework)
 const express = require('express');
 const app = express();
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-// ejs 설정
+// npm install ejs (view)
 app.set('view engine', 'ejs');
 
-// form 에서 PUT, DELETE 사용 -> 메소드 오버라이드
+// npm install passport passport-local express-session (세션 로그인)
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const session = require('express-session');
+
+app.use(session({ secret: 'secretKey', resave: true, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// npm install method-override --save (form 에서 PUT, DELETE 사용)
 const methodOverride = require('method-override')
 app.use(methodOverride('_method'))
 
-// mongodb 설정
+// npm install mongodb
 const MongoClient = require('mongodb').MongoClient;
+const res = require('express/lib/response');
 var db;
-// DB 레플리케이션 or 샤딩할 경우 여러개 사용해야 할 듯
-MongoClient.connect('mongodb+srv://kihwan:tptkddp12!@cluster0.usvux.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', function(err,client){
+// DB 샤딩 및 복제할 경우 어떻게 할 것 인가?
+MongoClient.connect(process.env.DB_URL, function(err, client) {
 
     if (err) return console.log(err)
     db = client.db('todoapp')
 
     // DB 연결 확인 후 app 시작
-    app.listen(8080, function(){
+    app.listen(process.env.PORT, function() {
         console.log('listen 8080')
     });
 })
 
+// 로컬스트레티지 인증방식 (세션인증)
+passport.use(new LocalStrategy({
+    usernameField: 'id',
+    passwordField: 'pw',
+    session: true,
+    passReqToCallback: false,
+}, function(usernameField, passwordField, done) {
+    //console.log(usernameField, passwordField);
+    db.collection('login').findOne({ id: usernameField }, function(err, res) {
+        if (err) return done(err)
+
+        if (!res) return done(null, false, { message: '그런 아이디 없음' })
+        if (passwordField == res.pw) {
+            return done(null, res)
+        } else {
+            return done(null, false, { message: '비번 틀렸는디?' })
+        }
+    })
+}));
+
+// 세션에 넣는 작업
+passport.serializeUser(function(user, done) {
+    done(null, user.id)
+});
+
+// 디비에서 데이터 가져오는 작업
+passport.deserializeUser(function(user, done) {
+    db.collection('login').findOne({ id: user }, function(err, res) {
+        done(null, res)
+    })
+
+});
+
+// 로그인 체크함수
+function checkLogin(req, res, next) {
+    if (req.user) {
+        next()
+    } else {
+        res.redirect('/login')
+    }
+}
+
 // public 폴더 static 메모리에 로드
 app.use('/public', express.static('public'));
 
-// HOME 화면
+// HOME
 app.get('/', function(req, res) {
     res.render('index.ejs')
 });
 
-// 글 작성화면
+// 작성화면
 app.get('/write', function(req, res) {
     // res.sendFile(__dirname + '/write.html')
     res.render('write.ejs')
 });
 
-// 게시물
+// 리스트
 app.get('/list', function(req, res) {
     db.collection('post').find().toArray(function(err, result) {
         console.log(result)
-        res.render('list.ejs', { posts : result });
+        res.render('list.ejs', { posts: result });
     });
 });
 
-// 글 저장하기
+// 저장하기
 app.post('/add', function(req, res) {
     // let today = new Date();
     // req.body._id = today.toLocaleString()
-    db.collection('counter').findOne({name : '게시물'}, function(err, res) {
+    db.collection('counter').findOne({ name: '게시물' }, function(err, res) {
         if (err) return console.log(err)
         req.body._id = res.totalPost + 1
-        db.collection('post').insertOne(req.body, function(err, res){
+        db.collection('post').insertOne(req.body, function(err, res) {
             if (err) return console.log(err)
-            db.collection('counter').updateOne({name : '게시물'},{ $inc : {totalPost : 1}}, function(err, res){
+            db.collection('counter').updateOne({ name: '게시물' }, { $inc: { totalPost: 1 } }, function(err, res) {
                 if (err) return console.log(err)
                 res.redirect('/list')
             })
@@ -64,35 +120,77 @@ app.post('/add', function(req, res) {
     });
 })
 
-// 글 삭제하기
+// 삭제하기
 app.delete('/delete/', function(req, res) {
     req.body._id = parseInt(req.body._id);
-    db.collection('post').deleteOne( req.body, function(err, response) {
+    db.collection('post').deleteOne(req.body, function(err, response) {
         if (err) return console.log(err);
-        res.status(200).send({ message : '삭제 성공' })
+        res.status(200).send({ message: '삭제 성공' })
     })
 })
 
-// 글 상세화면
+// 상세화면
 app.get('/detail/:id', function(req, res) {
-    db.collection('post').findOne({_id : parseInt(req.params.id)}, function(err, response) {
+    db.collection('post').findOne({ _id: parseInt(req.params.id) }, function(err, response) {
         if (err) return console.log(err);
         console.log(response)
-        res.render('detail.ejs', { post : response })
+        res.render('detail.ejs', { post: response })
     })
 })
 
+// 수정하기
 app.get('/edit/:id', function(req, res) {
-    db.collection('post').findOne( {_id : parseInt(req.params.id)}, function(err, response){
+    db.collection('post').findOne({ _id: parseInt(req.params.id) }, function(err, response) {
         if (err) return console.log(err);
         console.log(response)
-        res.render('edit.ejs', { post : response } )
+        res.render('edit.ejs', { post: response })
     })
 })
 
 app.put('/edit', function(req, res) {
-    db.collection('post').updateOne( { _id : parseInt(req.body.id) }, { $set : { title : req.body.title, date : req.body.date } },
-         function(err, response) {
+    db.collection('post').updateOne({ _id: parseInt(req.body.id) }, { $set: { title: req.body.title, date: req.body.date } },
+        function(err, response) {
             res.redirect('/list')
+        })
+})
+
+// 로그인 페이지
+app.get('/login', function(req, res) {
+    res.render('login.ejs')
+})
+
+app.post('/login', passport.authenticate('local', {
+    failureRedirect: '/fail'
+}), function(req, res) {
+    res.redirect('/')
+})
+
+// 마이페이지
+app.get('/mypage', checkLogin, function(req, res) {
+    console.log(req.user)
+    res.render('mypage.ejs', { user: req.user })
+})
+
+app.get('/search', (req, res) => {
+    var searchKeywords = [{
+            $search: {
+                index: 'titleSearch',
+                text: {
+                    query: req.query.value,
+                    path: 'title' // 제목날짜 둘다 찾고 싶으면 ['제목', '날짜']
+                }
+            }
+        },
+        // 결과를 정렬
+        { $sort: { _id: 1 } },
+        // sql limit과 같음
+        { $limit: 10 },
+        // 0은 안보여주고 1은 보여줌
+        { $project: { title: 1, _id: 0 } }
+    ]
+
+    console.log(req.query.value)
+    db.collection('post').aggregate(searchKeywords).toArray((err, result) => {
+        console.log(result)
     })
 })
